33906641181933662138fb81d4c9005c12731357
mcode8 <laxmipalkant01@gmail.com>
2023-05-27 12:37:39 +0530

    origin states

---
+++ nanosrc/src/main/java/com/sangeet/nanosrc/Nano.kt
@@ -0,0 +1,26 @@
+package com.sangeet.nanosrc
+
+object Nano {
+
+    // PlayList
+    const val SRC1_PLAYLIST ="https://www.youtube.com/playlist?"
+    const val SRC2_PLAYLIST ="https://youtube.com/playlist?"
+    const val SRC3_PLAYLIST ="https://music.youtube.com/playlist?"
+    const val SRC4_PLAYLIST ="https://m.youtube.com/playlist?"
+
+    const val PLAYER_WATCH ="https://www.youtube.com/watch?v="
+
+    const val WATCH_API ="https://watchapi.whatever.social/streams/"
+
+    //
+    const val KEY_APP ="X-Goog-Api-Key"
+    const val KEY_VALUE_APP ="AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8"
+
+    //
+    const val MEDIA_ITEM_MENU ="https://music.youtube.com/watch?v="
+
+    const val PLAYLIST_SONGLIST="https://music.youtube.com/playlist?list="
+
+    const val BROUSE_ID ="https://music.youtube.com/channel/"
+
+}
